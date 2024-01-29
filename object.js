7cb0095e3dd43eda51db50aec3213ed12b31be08
TOOBASALEEM2 <157893203+TOOBASALEEM2@users.noreply.github.com>
2024-01-29 19:01:44 +0500

    Delete object.js
    ok

--- object.js
+++
@@ -1,9 +0,0 @@
-let object  = {
-    fname : "tooba",
-    lname : "saleem",
-    age : "12",
- }
- //console.log(object)
- document.write(Object.keys(object))
- document.write(Object.values(object))
- document.write(Object.entries(object))
