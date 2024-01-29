9947f86f9cbae5df0cfb3212a2a7d0cabf18a38a
TOOBASALEEM2 <157893203+TOOBASALEEM2@users.noreply.github.com>
2024-01-29 19:35:10 +0500

    Add files via upload
    ok

---
+++ operators.js
@@ -0,0 +1,18 @@
+        // Assign the value 5 to x
+        let x = 5;
+        // Assign the value 2 to y
+        let y = 2;
+        // Assign the value x + y to z
+        let z = x + y;
+        // Display z
+        document.write(x)
+
+        /*<h1>Types of js operators</h1>
+        <h3>Arithmetic Operators</h3>
+          <h3>Assignment Operators</h3>
+          <h3> Comparison Operators</h3>
+          <h3> String Operators</h3>
+            <h3> Logical Operators</h3>
+                <h3> Bitwise Operators</h3>
+                    <h3> Ternary Operators</h3>
+                        <h3>Type Operators</h3>*/
