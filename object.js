9dffd03b034f550a0346075a7b3de88d6112696c
TOOBASALEEM2 <157893203+TOOBASALEEM2@users.noreply.github.com>
2024-01-29 19:02:15 +0500

    Add files via upload
    ok

---
+++ object.js
@@ -0,0 +1,34 @@
+//let object  = {
+    //fname : "tooba",
+  //  lname : "saleem",
+   // age : "12",
+// }
+ //console.log(object)
+// document.write(Object.keys(object))
+ //document.write(Object.values(object))
+ //document.write(Object.entries(object))
+
+ //nested object
+
+// const company = {
+  //  smartphones: {
+    //    Samsung: {
+      //      samsungNoteSeries: {
+        //        samsungNote10 : {
+          //          "Ram" : "8gb",
+            //        Storage : "128gb"
+             //   },
+
+              //  samsungNote10s : {
+                //    "Ram" : "12gb",
+                  //  Storage : "256gb"
+                //}
+
+           // }
+       // }
+   // }
+//}
+
+//document.write(Object.keys(company.smartphones.Samsung.samsungNoteSeries.samsungNote10))
+//document.write(Object.values(company.smartphones.Samsung.samsungNoteSeries.samsungNote10))
+//document.write(Object.entries(company.smartphones.Samsung.samsungNoteSeries.samsungNote10))
