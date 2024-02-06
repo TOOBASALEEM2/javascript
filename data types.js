b6085af5ae17c81c46f6d096c5ec59466b07fe6f
toobasaleem <157893203+TOOBASALEEM2@users.noreply.github.com>
2024-02-06 16:19:33 +0500

    Add files via upload
    ok

---
+++ data types.js
@@ -0,0 +1,24 @@
+//different types of data types
+
+// var x = "I am tooba"; ITS a string data type
+
+//var x = 25; its a number data type
+
+//var x = true; its a boolean data type
+
+//var x = ["zara","zainab","sara"] its a array data type
+
+//var x = {first:"deena",last:"majid"}its a object data type
+
+//var x = null; its a null data type
+
+//var x; its a undefined data type
+
+var x = "y";
+x = -34.50;
+x = false;
+x = undefined;
+var z;
+document.write(x);
+document.write("<br>");
+document.write(typeof x);
